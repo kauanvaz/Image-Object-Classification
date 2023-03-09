96eff5c7653641afc3e6ea708d194d2b4a52986b
Kauan Vaz do Nascimento <kauan.nascimento@tce.pi.gov.br>
2023-03-09 12:42:46 -0300

    Filter and threshold

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 # Load the image in grayscale
-image = cv2.imread('gears.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv.imread('gears.jpg')
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Filter the image with a 5x5 gaussian kernel to remove possible noise
+blurred = cv.GaussianBlur(gray, (5, 5), 0)
+
+# Apply Otsu's threshold method (automatically determines the threshold value)
+ret, thresh = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+cv.imshow("Threshold", thresh)
+cv.waitKey(0)

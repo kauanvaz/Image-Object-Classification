ea9704c025a2de780a0b061f9531ad11505ad1f8
Kauan Vaz do Nascimento <kauan.nascimento@tce.pi.gov.br>
2023-03-22 08:53:52 -0300

    threshould before histogram

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ def draw_label(img, x, y, w, h, color, label):
     cv.putText(img, label, (x+10,y+15), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 # Load the image in grayscale
-image = cv.imread('images/gears.jpg')
+image = cv.imread('images/stars.jpg')
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 # Filter the image with a 5x5 gaussian kernel to remove possible noise
 blurred = cv.GaussianBlur(gray, (5, 5), 0)
 
+# Apply Otsu's threshold method (automatically determines the threshold value)
+_, thr_img = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
 # Generate the image's histogram
-hist = cv.calcHist([blurred], [0], None, [256], [0, 256])
+hist = cv.calcHist([thr_img], [0], None, [256], [0, 256])
 
 # If there are more white pixels than black pixels, it means the background is white and objects are black, so colors are inverted
 # because the findContours function assumes that objects in the image are lighter than the background by default
-img_for_thres = blurred
+img_for_contours = thr_img
 if hist[-1] > hist[0]:
     # Invert the colors of the image (detect white objects in a black background)
-    img_for_thres = cv.bitwise_not(blurred)
+    img_for_contours = cv.bitwise_not(thr_img)
 
-# Apply Otsu's threshold method (automatically determines the threshold value)
-_, thr_img = cv.threshold(img_for_thres, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-contours, hierarchy = cv.findContours(thr_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(img_for_contours, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
 # Store each area of contours
 areas = []
